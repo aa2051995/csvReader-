215c2ce5ae7845af551e79814eb04028ef547781
ahmedmansour <a.a2051995@gmail.com>
2021-05-25 15:40:13 +0200

    make csv read in seperate thread

--- Lab2/src/PyramidsCSVImp.java
+++ Lab2/src/PyramidsCSVImp.java
@@ -12,12 +12,10 @@ import java.util.Locale;
 import com.sun.source.tree.WhileLoopTree;
 
 public class PyramidsCSVImp  implements PyramidCSVDAO{
-	private List<Pyramid> pyramids;
 public PyramidsCSVImp() {
-	pyramids = new ArrayList<Pyramid>();
 }
 	@Override
-	public List<Pyramid> readCsv(String csvpath) throws IOException {
+	public List<Pyramid> readCsv(List<Pyramid> pyramids,String csvpath) throws IOException {
 		File file = new File(csvpath);
         FileReader fileReader = new FileReader(file);
         BufferedReader bufferedReader = new BufferedReader(fileReader);
@@ -34,7 +32,7 @@ public PyramidsCSVImp() {
 	        	if(line!= null)
 	        	{
 	        		String cols[] = line.split(",",-1);
-	        		System.out.println(line);
+	        		//System.out.println(line);
 	        		try {
 						pyramids.add(createPyramid(cols));
 					} catch (NumberFormatException | ParseException e) {
@@ -57,21 +55,21 @@ public PyramidsCSVImp() {
 	@Override
 	public Pyramid createPyramid(String[] meta) throws NumberFormatException, ParseException {
 		Pyramid pyramid =  new Pyramid(meta[0],
-			meta[1], 
-			meta[2],
-			Double.valueOf(meta[3]),
-			meta[4],
-			NumberFormat.getInstance(Locale.GERMAN).parse(meta[5] != "" ? meta[5] : "-1").doubleValue(), 
-			NumberFormat.getInstance(Locale.GERMAN).parse(meta[6] != "" ? meta[6] : "-1").doubleValue(),
-			NumberFormat.getInstance(Locale.GERMAN).parse(meta[7] != "" ? meta[7] : "-1").doubleValue(), 
-			NumberFormat.getInstance(Locale.GERMAN).parse(meta[8] != "" ? meta[8] : "-1").doubleValue(),
-			NumberFormat.getInstance(Locale.GERMAN).parse(meta[9] != "" ? meta[9] : "-1").doubleValue(), 
-			NumberFormat.getInstance(Locale.GERMAN).parse(meta[10] != "" ? meta[10] : "-1").doubleValue(),
-			NumberFormat.getInstance(Locale.GERMAN).parse(meta[11] != "" ? meta[11] : "-1").doubleValue(), 
-			meta[12], 
-			meta[13],
-			meta[14], 
-			meta[15]);
+				meta[1], 
+				meta[2],
+				Double.valueOf(meta[3]),
+				meta[4],
+				NumberFormat.getInstance(Locale.GERMAN).parse(meta[5] != "" ? meta[5] : "-1").doubleValue(), 
+				NumberFormat.getInstance(Locale.GERMAN).parse(meta[6] != "" ? meta[6] : "-1").doubleValue(),
+				NumberFormat.getInstance(Locale.GERMAN).parse(meta[7] != "" ? meta[7] : "-1").doubleValue(), 
+				NumberFormat.getInstance(Locale.GERMAN).parse(meta[8] != "" ? meta[8] : "-1").doubleValue(),
+				NumberFormat.getInstance(Locale.GERMAN).parse(meta[9] != "" ? meta[9] : "-1").doubleValue(), 
+				NumberFormat.getInstance(Locale.GERMAN).parse(meta[10] != "" ? meta[10] : "-1").doubleValue(),
+				NumberFormat.getInstance(Locale.GERMAN).parse(meta[11] != "" ? meta[11] : "-1").doubleValue(), 
+				meta[12], 
+				meta[13],
+				meta[14], 
+				meta[15]);
 		return pyramid;
 	}
 
