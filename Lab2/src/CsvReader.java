201f146964748d0b679c72206e4164ad579d6a94
ahmedmansour <a.a2051995@gmail.com>
2021-05-25 15:41:38 +0200

    refctoring main class method to testing only

--- Lab2/src/CsvReader.java
+++ Lab2/src/CsvReader.java
@@ -1,20 +1,26 @@
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
 
 public class CsvReader {
 public static void main(String[] args) {
-	PyramidsCSVImp pyramidsCSVImp = new PyramidsCSVImp();
-	List<Pyramid> pyramids;
-	try {
-		pyramids = pyramidsCSVImp.readCsv("C:\\Users\\Ahmed mansour\\Downloads\\pyramids.csv");
+	
+	    List<Pyramid> pyramids = pyramidsManpulation.readCsv();
+	    
 		int i =0;
+		pyramidsManpulation.Sort(pyramids);
 		for(Pyramid pyramid: pyramids) {
 			System.out.println("#   "+(++i)+" "+pyramid);
 		}
-	} catch (IOException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	}
+		
+		Map<String, Integer> sites = pyramidsManpulation.buildPyramidsMap(pyramids);
+		for(String numsofsites : sites.keySet()) {
+			System.out.println("site: " +numsofsites+" "+sites.get(numsofsites));
+		}
+	
 	
 }
+
 }
